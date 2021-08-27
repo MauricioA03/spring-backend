641f115a7b34bcb5c89af6cd33d928e0c00a73bd
MauricioArancibia <mauricio.crispink@gmail.com>
2021-08-27 13:41:01 -0400

    Adicionar @Entity and adiciconar las relaciones de los entitys

--- src/main/java/com/sales/market/model/Position.java
+++ src/main/java/com/sales/market/model/Position.java
@@ -1,22 +1,11 @@
 package com.sales.market.model;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
 
 //Cajero, Acomodador,Almacenamiento
 @Entity
-public class Position {
-    @Id
-    private Long id;
+public class Position extends ModelBase {
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
