201044e6d451fbe5d678234e38de623cd14d3d49
MauricioArancibia <mauricio.crispink@gmail.com>
2021-08-30 22:01:20 -0400

    DTO con template

--- src/main/java/com/sales/market/dto/BuyDto.java
+++ src/main/java/com/sales/market/dto/BuyDto.java
@@ -17,3 +17,5 @@ public class BuyDto extends DtoBase<Buy> {
         this.value = value;
     }
 }
+
+
--- src/main/java/com/sales/market/model/Buy.java
+++ src/main/java/com/sales/market/model/Buy.java
@@ -19,3 +19,4 @@ public class Buy extends ModelBase<BuyDto> {
         this.value = value;
     }
 }
+
