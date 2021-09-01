72729f15c1d1807e2d304d26a5d2172ed0a9189f
MauricioArancibia <mauricio.crispink@gmail.com>
2021-09-01 12:46:24 -0400

    adicion de configuracion para JPA auditing

---
+++ src/main/java/com/sales/market/config/PersistenceConfig.java
@@ -0,0 +1,15 @@
+package com.sales.market.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories
+@EnableTransactionManagement
+public class PersistenceConfig {
+
+
+}
