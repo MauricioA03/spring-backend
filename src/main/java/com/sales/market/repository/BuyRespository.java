b069aaa32e6e0cea75d61a656a1c79d3551c7c35
MauricioArancibia <mauricio.crispink@gmail.com>
2021-09-01 14:47:24 -0400

    add repositories

---
+++ src/main/java/com/sales/market/repository/BuyRespository.java
@@ -0,0 +1,9 @@
+package com.sales.market.repository;
+
+import com.sales.market.model.Buy;
+
+import java.math.BigDecimal;
+
+public interface BuyRespository extends GenericRepository<Buy> {
+    public Buy findAllByValue(BigDecimal value);
+}
