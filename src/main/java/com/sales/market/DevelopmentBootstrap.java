7ee093015a801d1fdf5f0529b5176118fb036b26
MauricioArancibia <mauricio.crispink@gmail.com>
2021-09-15 21:20:52 -0400

    support Item, Iteminstance, Itemservice, Itemcontroller

--- src/main/java/com/sales/market/DevelopmentBootstrap.java
+++ src/main/java/com/sales/market/DevelopmentBootstrap.java
@@ -1,17 +1,19 @@
 package com.sales.market;
 
-import com.sales.market.model.Buy;
-import com.sales.market.model.Category;
-import com.sales.market.model.Employee;
-import com.sales.market.model.SubCategory;
+import com.sales.market.model.*;
 import com.sales.market.repository.BuyRepository;
 import com.sales.market.repository.EmployeeRepository;
 import com.sales.market.service.CategoryService;
+import com.sales.market.service.ItemInstanceService;
+import com.sales.market.service.ItemService;
 import com.sales.market.service.SubCategoryService;
+import com.sales.market.util.ImageUtils;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigDecimal;
 
 @Component
@@ -20,15 +22,22 @@ public class DevelopmentBootstrap implements ApplicationListener<ContextRefreshe
     private final CategoryService categoryService;
     private final SubCategoryService subCategoryService;
     private EmployeeRepository employeeRepository;
+    private final ItemService itemService;
+    private final ItemInstanceService itemInstanceService;
+
+    SubCategory beverageSubCat = null;
 
     // injeccion evita hacer instancia   = new Clase();
     // bean pueden tener muchos campos y otros beans asociados
     public DevelopmentBootstrap(BuyRepository buyRepository, CategoryService categoryService,
-                                SubCategoryService subCategoryService, EmployeeRepository employeeRepository) {
+                                SubCategoryService subCategoryService, EmployeeRepository employeeRepository,
+                                ItemService itemService, ItemInstanceService itemInstanceService) {
         this.buyRepository = buyRepository;
         this.categoryService = categoryService;
         this.subCategoryService = subCategoryService;
         this.employeeRepository = employeeRepository;
+        this.itemService = itemService;
+        this.itemInstanceService = itemInstanceService;
     }
 
     @Override
@@ -39,12 +48,55 @@ public class DevelopmentBootstrap implements ApplicationListener<ContextRefreshe
         persistEmployee("Juan", "Mecanico");
         persistEmployee("Sandra", "Renan");
         persistCategoriesAndSubCategories();
+        Item maltinItem = persistItems(beverageSubCat);
+        persistItemInstances(maltinItem);
+    }
+
+    private void persistItemInstances(Item maltinItem) {
+        ItemInstance maltinItem1 = createItem(maltinItem, "SKU-77721106006158", 5D);
+        ItemInstance maltinItem2 = createItem(maltinItem, "SKU-77721106006159", 5D);
+        ItemInstance maltinItem3 = createItem(maltinItem, "SKU-77721106006160", 5D);
+        ItemInstance maltinItem4 = createItem(maltinItem, "SKU-77721106006161", 5D);
+        itemInstanceService.save(maltinItem1);
+        itemInstanceService.save(maltinItem2);
+        itemInstanceService.save(maltinItem3);
+        itemInstanceService.save(maltinItem4);
+    }
+
+    private ItemInstance createItem(Item maltinItem, String sku, double price) {
+        ItemInstance itemInstance = new ItemInstance();
+        itemInstance.setItem(maltinItem);
+        itemInstance.setFeatured(true);
+        itemInstance.setPrice(price);
+        itemInstance.setIdentifier(sku);
+        return itemInstance;
+    }
+
+    private Item persistItems(SubCategory subCategory) {
+        Item item = new Item();
+        item.setCode("B-MALTIN");
+        item.setName("MALTIN");
+        item.setSubCategory(subCategory);
+        try {
+            item.setImage(ImageUtils.inputStreamToByteArray(getResourceAsStream("/images/maltin.jpg")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return itemService.save(item);
+    }
+
+    private InputStream getResourceAsStream(String resourceName) {
+        try (InputStream inputStream = this.getClass().getResourceAsStream(resourceName)) {
+            return inputStream;
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
     }
 
     private void persistCategoriesAndSubCategories() {
         Category category = persistCategory();
         persistSubCategory("SUBCAT1-NAME", "SUBCAT1-CODE", category);
-        persistSubCategory("SUBCAT2-NAME", "SUBCAT2-CODE", category);
+        beverageSubCat = persistSubCategory("BEVERAGE", "BEVERAGE-CODE", category);
     }
 
     private Category persistCategory() {
@@ -54,12 +106,12 @@ public class DevelopmentBootstrap implements ApplicationListener<ContextRefreshe
         return categoryService.save(category);
     }
 
-    private void persistSubCategory(String name, String code, Category category) {
+    private SubCategory persistSubCategory(String name, String code, Category category) {
         SubCategory subCategory = new SubCategory();
         subCategory.setName(name);
         subCategory.setCode(code);
         subCategory.setCategory(category);
-        subCategoryService.save(subCategory);
+        return subCategoryService.save(subCategory);
     }
 
     private void persistEmployee(String firstName, String lastName) {
